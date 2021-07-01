a9b7b3d6ab04ab5b58e516b069ad206eeaf8bf06
Ice <xieshan4@gmail.com>
2021-07-01 23:34:02 +1000

    Added encrypt but neither add a single line nor remove a  single line

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ if (len(sys.argv) < 2):
   sys.exit(1)
 
 filename = sys.argv[1]
-dest = os.path.splitext(filename)[0] + '.zip'
+dest = os.path.splitext(filename)[0] + ('.zip' if (isDecrypt := os.path.splitext(filename)[1] == '.dyn') else '.dyn')
 
-# XOR key and shuffle index arrays for decryption
+# XOR key and shuffle index arrays for convert
 CHUNK_SIZE = 8
 key = bytearray([(0x8D + i) for i in range(CHUNK_SIZE)])
 indices = [5, 3, 6, 7, 4, 2, 0, 1]
@@ -28,17 +28,17 @@ with open(filename, 'rb') as file, open(dest, 'wb') as output:
     if not chunk:
       break
 
-    # Need a full 8-byte chunk to decrypt.
+    # Need a full 8-byte chunk to convert.
     if(len(chunk) == CHUNK_SIZE):
       out = bytearray(CHUNK_SIZE)
 
-      # Decrypt the chunk.
+      # Convert the chunk.
       for i in range(CHUNK_SIZE):
-        out[i] = chunk[indices[i]] ^ key[i]
+        out[i if isDecrypt else indices[i]] = chunk[indices[i] if isDecrypt else i] ^ key[i]
     else:
       out = chunk
 
     debug('out = {}'.format(out))
     output.write(out)
 
-print('Decrypted {} to {}'.format(filename, dest))
+print('{} {} to {}'.format('Decrypted' if isDecrypt else 'Encrypted', filename, dest))
