224288d336e086728677172feb62d90853391508
Ice <xieshan4@gmail.com>
2021-07-03 01:29:11 +1000

    Bug fix but also neither added a single line nor removed a single line
    Fixed a bug that caused by converted block that should not be converted
    e.g. axe_victorian.dyn

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ filename = sys.argv[1]
 dest = os.path.splitext(filename)[0] + ('.zip' if (isDecrypt := os.path.splitext(filename)[1] == '.dyn') else '.dyn')
 
 # XOR key and shuffle index arrays for convert
-CHUNK_SIZE = 8
-key = bytearray([(0x8D + i) for i in range(CHUNK_SIZE)])
-indices = [5, 3, 6, 7, 4, 2, 0, 1]
+CHUNK_SIZE = 16
+key = bytearray([(0x8D + i%8) for i in range(CHUNK_SIZE)])
+indices = (indices := [5, 3, 6, 7, 4, 2, 0, 1]) + [i+8 for i in indices]
 
 debug('chunk size = {}, key = {}, indices = {}'.format(CHUNK_SIZE, key, indices))
 debug('filename = {}, dest = {}'.format(filename, dest))
@@ -29,7 +29,7 @@ with open(filename, 'rb') as file, open(dest, 'wb') as output:
       break
 
     # Need a full 8-byte chunk to convert.
-    if(len(chunk) == CHUNK_SIZE):
+    if len(chunk) == CHUNK_SIZE:
       out = bytearray(CHUNK_SIZE)
 
       # Convert the chunk.
